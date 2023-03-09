59933d55bed1d03b2d2600553773a35e94812ebe
James Yang <78498381+polaryang@users.noreply.github.com>
2023-03-09 11:50:11 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,2 @@
 import streamlit as st
-import matplotlib.pyplot as plt
-
-#create your figure and get the figure object returned
-fig = plt.figure() 
-plt.plot([1, 2, 3, 4, 5]) 
-
-st.pyplot(fig)
+st.write(hello world")
