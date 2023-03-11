c24d9e009094fa3b6d545d983f5622c4920abf49
James Yang <78498381+polaryang@users.noreply.github.com>
2023-03-11 20:41:37 +0800

    Update test.py

--- test.py
+++ test.py
@@ -27,8 +27,9 @@ y=data_h.Close
 x=data_h.index
 plt.plot(x,y,linestyle=':',color='b')
 plt.title('Stock Price')
-#rotate x-axis tick labels
+rotate x-axis tick labels
 plt.xticks(rotation=45, ha='right')
 #st.pyplot(fig)
 fig_html = mpld3.fig_to_html(fig)
 components.html(fig_html, height=600)
+
