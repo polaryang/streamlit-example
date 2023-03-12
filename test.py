fe123ad5c95b42ac906663381957ebb3d34b0d8e
James Yang <78498381+polaryang@users.noreply.github.com>
2023-03-12 08:48:18 +0800

    Update test.py

--- test.py
+++ test.py
@@ -5,10 +5,6 @@ import mpld3
 import streamlit.components.v1 as components
 import matplotlib.pyplot as plt
 
-option = st.selectbox(
-     'How would you like to be contacted?',
-     ('Email', 'Home phone', 'Mobile phone'))
-st.write('You selected:', option)
 d = st.date_input(
      'Starting Date:',
      datetime.date(2018, 1, 1))
@@ -17,13 +13,21 @@ st.write('Starting Date:', d)
 today = datetime.date.today()
 stock_ticker=st.text_input('Input Ticker','2330.TW')
 data_h = yf.download(stock_ticker, start=d, end=today, interval="1d")
-data_r = data_h.pct_change()
-#create figure
-fig=plt.figure()
+
+option = st.selectbox(
+     'What kind of information you want to see?',
+     ('Stock Price', 'Return(%)'))
+st.write('You selected:', option)
+
 y=data_r.Close
 x=data_r.index
+if option='Return(%)':
+     data_r = data_h.pct_change()
+
+#create figure
+fig=plt.figure()
 plt.plot(x,y,linestyle='-',color='b')
-plt.title('Stock Price')
+plt.title('Stock '+stock_ticker+' '+option)
 #rotate x-axis tick labels
 plt.xticks(rotation=45, ha='right')
 #st.pyplot(fig)
