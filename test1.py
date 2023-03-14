15b1025b1458a26e1c11d7970d782b01c5faa328
James Yang <78498381+polaryang@users.noreply.github.com>
2023-03-15 01:20:00 +0800

    Update test1.py

--- test1.py
+++ test1.py
@@ -60,7 +60,7 @@ with col2:
     
   with tab3:
     fig = qs.plots.snapshot(df_ret.Close, title='Facebook Performance',savefig='sdfs.png')
-    matrix = qs.reports.metrics(df_ret.Close,benchmark=bmk_ret,mode='full', display=False)
+    matrix = qs.reports.metrics(df_ret.Close,benchmark=bmk_ret.Close,mode='full', display=False)
     st.dataframe(matrix)
     #st.plotly_chart(fig,use_container_width=True)
   
