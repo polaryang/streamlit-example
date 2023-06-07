68488a2829fa404220e48771531108ef43f837b3
James Yang <78498381+polaryang@users.noreply.github.com>
2023-06-07 23:16:20 +0800

    Update ttt.py

--- ttt.py
+++ ttt.py
@@ -36,7 +36,6 @@ def Checking_ID(ID):
               ID_mkt=stories[i+4].text #market
               ID_type=stories[i+5].text #type
     return ID_code, ID_name, ID_mkt, ID_type
-    break
               #return ID_code,ID_name,ID_type    #return ID number
   except:
     no_found=1
