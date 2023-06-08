5d1202c20c0f3779e8b5ec279a887117e2da4a10
James Yang <78498381+polaryang@users.noreply.github.com>
2023-06-09 00:37:18 +0800

    Update ttt.py

--- ttt.py
+++ ttt.py
@@ -161,15 +161,13 @@ with col1:
   if ID_mkt=='上櫃 ':
     stock_ticker=ID_code+'.TWO'
   st.write(ID_name+' : '+stock_ticker)
-  data = yf.Ticker(stock_ticker)
-  divid=data.dividends
-  divid=data.dividends
-  splits=data.splits
-  last_close=data.history()['Close'].tail().mean() # 最近5日平均收盤價
-  st.write('Avg Price : '+str(last_close))
+
 # ------------------------------------------------------------------
-
-
+data = yf.Ticker(stock_ticker)
+divid=data.dividends
+splits=data.splits
+last_close=data.history()['Close'].tail().mean() # 最近5日平均收盤價
+st.write('Avg Price : '+str(last_close))
 income_a=income*(12+income_bonus)
 expense_a=expense*12
 years=pd.Series(data.dividends.index.year)
