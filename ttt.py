906c6fe33bc7af60525fa6a5101ce3be08243fc2
James Yang <78498381+polaryang@users.noreply.github.com>
2023-06-09 00:48:41 +0800

    Update ttt.py

--- ttt.py
+++ ttt.py
@@ -167,7 +167,7 @@ income_a=income*(12+income_bonus)
 expense_a=expense*12
 data = yf.Ticker(stock_ticker)
 divid=data.dividends
-st.write(length(divid))
+st.dataframe(divid)
 splits=data.splits
 last_close=data.history()['Close'].tail().mean() # 最近5日平均收盤價
 #st.write('Avg Price : '+str(last_close))
