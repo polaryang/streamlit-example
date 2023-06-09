02f034f549db7a31473cb93bf64216e670355b7c
James Yang <78498381+polaryang@users.noreply.github.com>
2023-06-09 11:34:25 +0800

    Update streamlit_app_wc.py

--- streamlit_app_wc.py
+++ streamlit_app_wc.py
@@ -1,28 +1,5 @@
-#from selenium import webdriver
-#from selenium.webdriver.chrome.service import Service as ChromeService
-#from webdriver_manager.chrome import ChromeDriverManager
-#options.add_argument('--no-sandbox')
-#options.add_argument('--window-size=1420,1080')
-#options.add_argument('--headless')
-#options.add_argument('--disable-gpu')
-#driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
-import pandas as pd
-import yfinance as yf
-import datetime
-import requests
-from bs4 import BeautifulSoup
-import matplotlib.pyplot as plt
-import streamlit as st
-import streamlit.components.v1 as components
-import altair as alt
-import math
-
-st.write('Testing')
-from webdriver_manager.core.utils import read_version_from_cmd, PATTERN
-version = read_version_from_cmd("/usr/bin/chrome-bin --version", PATTERN["chrome"])
-driver_binary = ChromeDriverManager(version=version).install()
-
-from selenium import webdriver  #從library中引入webdriver
+from selenium.webdriver import Chrome
+driver = Chrome()
 #ChromeDriverManager(path = r".\\Drivers").install()
 #https://github.com/polaryang/streamlit-example/blob/886d0c51c8668fbee9fa1a4a7efdceba7b764908/test1.py
 options=webdriver.ChromeOptions()
@@ -31,7 +8,7 @@ options.add_argument('--window-size=1420,1080')
 options.add_argument('--headless')
 options.add_argument('--disable-gpu')
 options.add_argument('--disable-dev-shm-usage')
-chromedriver="//github.com/polaryang/streamlit-example/blob/886d0c51c8668fbee9fa1a4a7efdceba7b764908/chromedriver.exe"
+chromedriver="//github.com/polaryang/streamlit-example/blob/886d0c51c8668fbee9fa1a4a7efdceba7b764908/chromedriver"
 #driver = webdriver.Chrome(ChromeDriverManager().install())
 #driver = webdriver.Chrome(executable_path=chromedriver, options=options)
-driver = webdriver.Chrome()
+#driver = webdriver.Chrome()
