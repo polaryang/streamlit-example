cd0698ec3bc878e8431490f2c61786c7b781c48c
James Yang <78498381+polaryang@users.noreply.github.com>
2023-06-09 11:16:59 +0800

    Update streamlit_app_wc.py

--- streamlit_app_wc.py
+++ streamlit_app_wc.py
@@ -16,11 +16,10 @@ import streamlit as st
 import streamlit.components.v1 as components
 import altair as alt
 import math
-from selenium import webdriver
 
 st.write('Testing')
-from webdriver_manager.chrome import ChromeDriverManager
-ChromeDriverManager(path = r".\\Drivers").install()
+from selenium import webdriver  #從library中引入webdriver
+#ChromeDriverManager(path = r".\\Drivers").install()
 #https://github.com/polaryang/streamlit-example/blob/886d0c51c8668fbee9fa1a4a7efdceba7b764908/test1.py
 options=webdriver.ChromeOptions()
 options.add_argument('--no-sandbox')
