a54a2b5fe42e17fb8fc21b2926ef0b87b5b45939
James Yang <78498381+polaryang@users.noreply.github.com>
2023-06-09 15:24:15 +0800

    Update streamlit_app_wc.py

--- streamlit_app_wc.py
+++ streamlit_app_wc.py
@@ -6,19 +6,38 @@ import streamlit as st
 
 # create a screenshot of the webpage
 @st.cache
-def screenshot(URL):
-    options = webdriver.ChromeOptions()
-    options.headless = True
-    driver = webdriver.Chrome(options=options)
-    driver.get(URL)
-    # Screenshot as a .png file with  selenium
-    S = lambda X: driver.execute_script('return document.body.parentNode.scroll' +X)
-    driver.set_window_size(S('Width'), S('Height'))
-    page_screen = driver.save_screenshot('page_screen.jpg')
-    # visualize the screenshot with streamlit
-    driver.quit()
-    return page_screen
+url = "https://goodinfo.tw/StockInfo/StockDividendPolicy.asp?STOCK_ID="+str(ID) #new moudle using pyppeteer
+def screenshot(url):
+options = webdriver.ChromeOptions()
+options.headless = True
+driver = webdriver.Chrome(options=options)
+driver.get(URL)
+n = 5
+item_count = 0
+max_length = d_length
+t_year=0
+finalList=' 年度 '+' 股利 '+'殖利率(%)'+' 填息日數 '
+# year 現金股利 股票股利 填息花費日數 年均	年均殖利率(%) 
+while True:  
+    d_year=driver.find_element_by_xpath('//*[@id="divDetail"]/table/tbody/tr['+str(n)+']/td[1]').text
+    cash_dividend=driver.find_element_by_xpath('//*[@id="divDetail"]/table/tbody/tr['+str(n)+']/td[4]').text
+    stock_dividend=driver.find_element_by_xpath('//*[@id="divDetail"]/table/tbody/tr['+str(n)+']/td[7]').text
+    payback_days=driver.find_element_by_xpath('//*[@id="divDetail"]/table/tbody/tr['+str(n)+']/td[11]').text
+    #avg_price=driver.find_element_by_xpath('//*[@id="divDetail"]/table/tbody/tr['+str(n)+']/td[16]').text
+    dividend_yield=driver.find_element_by_xpath('//*[@id="divDetail"]/table/tbody/tr['+str(n)+']/td[19]').text
+    #print(d_year, cash_dividend,stock_dividend,dividend_yield,payback_days)
+    if d_year=='累計' or item_count>=max_length:
+        break
+    else:
+        n+=1
+        item_count+=1    
+        if str(d_year)!='∟':
+            t_year==d_year
+            len(cash_dividend)
+            finalList+='\n' + ' ' + d_year+' '+l_fit(cash_dividend)+' '+l_fit(dividend_yield)+' '+ r_fit(payback_days)
+#print(d_year, cash_dividend,stock_dividend,payback_days,avg_price,dividend_yield) 
+#return finalList,item_count
+st.write(d_year, cash_dividend,stock_dividend)
 
 
-URL='tw.yahoo.com'
-screenshot(URL)
+
