92f19e8ffdda3b3006676d0690e0b945b4de55ed
James Yang <78498381+polaryang@users.noreply.github.com>
2023-06-09 19:12:52 +0800

    Update qs_demo.py

--- qs_demo.py
+++ qs_demo.py
@@ -11,3 +11,16 @@ bmk=yf.download(benchmark, period='10y')['Adj Close'].pct_change()
 benchmark=pd.Series(data=bmk, index=df_ret.index).fillna(0)
 qs.extend_pandas()
 qs.plots.snapshot(df_ret, title='Facebook Performance')
+
+import pandas as pd
+id='00779B'
+df = pd.read_excel("EFT_Dividend.xlsx")
+df1=df[df['代碼']==id]
+print(df1)
+years=['2018', '2019', '2020', '2021', '2022']
+divid_list=[]
+for i in range(8,3,-1):
+    #print(i)
+    divid_list.append(df1.iloc[0, i])
+df_etf=pd.DataFrame(divid_list, index = years, columns =['Dividends'])
+df_etf.dropna()
