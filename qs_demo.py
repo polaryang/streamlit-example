6a07cc9fb7825a72190c20c1ee797441ae34339b
James Yang <78498381+polaryang@users.noreply.github.com>
2023-06-09 19:41:52 +0800

    Update qs_demo.py

--- qs_demo.py
+++ qs_demo.py
@@ -17,8 +17,9 @@ import pandas as pd
 import requests
 id='00779B' #https://github.com/polaryang/streamlit-example/blob/08f2526337ec7dd9ff5e951ffc5c18c543f1f4fc/EFT_Dividend.xlsx
 url= 'https://github.com/polaryang/streamlit-example/blob/08f2526337ec7dd9ff5e951ffc5c18c543f1f4fc/EFT_Dividend.xlsx'
+url='https://github.com/polaryang/streamlit-example/raw/08f2526337ec7dd9ff5e951ffc5c18c543f1f4fc/EFT_Dividend.xlsx'
 myfile = requests.get(url)
-st.write(myfile.text)
+#st.write(myfile.text)
 df = pd.read_excel(myfile.content)
 df1=df[df['代碼']==id]
 print(df1)
