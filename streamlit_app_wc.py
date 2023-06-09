3a02a09521a44b79046875023f6be7b19053f4b7
James Yang <78498381+polaryang@users.noreply.github.com>
2023-06-09 10:59:17 +0800

    Update streamlit_app_wc.py

--- streamlit_app_wc.py
+++ streamlit_app_wc.py
@@ -19,4 +19,9 @@ import math
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 st.write('Testing')
+options=webdriver.ChromeOptions()
+options.add_argument('--no-sandbox')
+options.add_argument('--window-size=1420,1080')
+options.add_argument('--headless')
+options.add_argument('--disable-gpu')
 driver = webdriver.Chrome(ChromeDriverManager().install())
