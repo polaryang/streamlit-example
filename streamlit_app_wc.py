9872e2ad0a264e6e7e662b8b93947cdbeed1802b
James Yang <78498381+polaryang@users.noreply.github.com>
2023-06-09 11:40:34 +0800

    Update streamlit_app_wc.py

--- streamlit_app_wc.py
+++ streamlit_app_wc.py
@@ -8,7 +8,7 @@ options.add_argument('--window-size=1420,1080')
 options.add_argument('--headless')
 options.add_argument('--disable-gpu')
 options.add_argument('--disable-dev-shm-usage')
-chromedriver="//github.com/polaryang/streamlit-example/blob/886d0c51c8668fbee9fa1a4a7efdceba7b764908/chromedriver"
+chromedriver="\\github.com\polaryang\streamlit-example\blob\886d0c51c8668fbee9fa1a4a7efdceba7b764908\chromedriver"
 #driver = webdriver.Chrome(ChromeDriverManager().install())
 #driver = webdriver.Chrome(executable_path=chromedriver, options=options)
 driver = webdriver.Chrome(chromedriver)
