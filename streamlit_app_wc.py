78f50ca770e87554298ca4843eae9557ef853c11
James Yang <78498381+polaryang@users.noreply.github.com>
2023-06-09 19:26:40 +0800

    Update streamlit_app_wc.py

--- streamlit_app_wc.py
+++ streamlit_app_wc.py
@@ -8,8 +8,8 @@ st.write("""
 """)
 parent_path = pathlib.Path(__file__).parent.parent.resolve()
 data_path = os.path.join(parent_path, "data")
-onlyfiles = [f for f in listdir(data_path) if isfile(join(data_path, f))]
-option = st.sidebar.selectbox('Pick a dataset', onlyfiles)
-file_location=os.path.join(data_path, option)
+# onlyfiles = [f for f in listdir(data_path) if isfile(join(data_path, f))]
+# option = st.sidebar.selectbox('Pick a dataset', onlyfiles)
+# file_location=os.path.join(data_path, option)
 # use `file_location` as a parameter to the main script
 st.write(file_location)
