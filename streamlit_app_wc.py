a723cc119d38f3f094fb6e18131ece8c14b164d9
James Yang <78498381+polaryang@users.noreply.github.com>
2023-06-09 20:13:44 +0800

    Update streamlit_app_wc.py

--- streamlit_app_wc.py
+++ streamlit_app_wc.py
@@ -13,3 +13,18 @@ data_path = os.path.join(parent_path, "data")
 # file_location=os.path.join(data_path, option)
 # use `file_location` as a parameter to the main script
 st.write(data_path)
+import pandas as pd
+import requests
+id='00779B' 
+myfile='https://github.com/polaryang/streamlit-example/raw/08f2526337ec7dd9ff5e951ffc5c18c543f1f4fc/EFT_Dividend.xlsx'
+df = pd.read_excel(myfile)
+df1=df[df['代碼']==id]
+print(df1)
+years=['2018', '2019', '2020', '2021', '2022']
+divid_list=[]
+for i in range(8,3,-1):
+    #print(i)
+    divid_list.append(df1.iloc[0, i])
+df_etf=pd.DataFrame(divid_list, index = years, columns =['Dividends'])
+df_etf=df_etf.dropna()
+st.dataframe(df_etf)
