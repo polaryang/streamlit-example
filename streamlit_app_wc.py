85d5282835d3ab84ca1a9e6873fb04c14ad09725
James Yang <78498381+polaryang@users.noreply.github.com>
2023-06-09 11:36:28 +0800

    Update streamlit_app_wc.py

--- streamlit_app_wc.py
+++ streamlit_app_wc.py
@@ -1,5 +1,5 @@
 from selenium.webdriver import Chrome
-driver = Chrome()
+from selenium import webdriver   
 #ChromeDriverManager(path = r".\\Drivers").install()
 #https://github.com/polaryang/streamlit-example/blob/886d0c51c8668fbee9fa1a4a7efdceba7b764908/test1.py
 options=webdriver.ChromeOptions()
@@ -11,4 +11,4 @@ options.add_argument('--disable-dev-shm-usage')
 chromedriver="//github.com/polaryang/streamlit-example/blob/886d0c51c8668fbee9fa1a4a7efdceba7b764908/chromedriver"
 #driver = webdriver.Chrome(ChromeDriverManager().install())
 #driver = webdriver.Chrome(executable_path=chromedriver, options=options)
-#driver = webdriver.Chrome()
+driver = webdriver.Chrome(chromedriver)
