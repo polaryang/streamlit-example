4e16bcd1481a17d570760eb522ad489aed1af4fa
James Yang <78498381+polaryang@users.noreply.github.com>
2023-06-27 22:11:49 +0800

    Update dividends_wealth_calc.py

--- dividends_wealth_calc.py
+++ dividends_wealth_calc.py
@@ -8,6 +8,32 @@ import streamlit as st
 import streamlit.components.v1 as components
 import altair as alt
 import math
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.header import Header
+from email.mime.application import MIMEApplication
+# ------------------------------------------------------------------
+def send_email('polaryang@gmail.com', 'it85391016', receiver, 'smtp.gmail.com', 
+587, 'fsdfsdf', 'test', attachment=None):
+#def send_email(sender, password, receiver, smtp_server, 
+#smtp_port, email_message, subject, attachment=None):  
+  message = MIMEMultipart()
+  message['To'] = Header(receiver)
+  message['From']  = Header(sender)
+  message['Subject'] = Header(subject)
+  message.attach(MIMEText(email_message,'plain', 'utf-8'))
+  if attachment:
+  att = MIMEApplication(attachment.read(), _subtype="txt")
+  att.add_header('Content-Disposition', 'attachment', filename=attachment.name)
+  message.attach(att)
+  server = smtplib.SMTP(smtp_server, smtp_port)
+  server.starttls()
+  server.ehlo()
+  server.login(sender, password)
+  text = message.as_string()
+  server.sendmail(sender, receiver, text)
+  server.quit()
 # ------------------------------------------------------------------
 def Checking_ID(ID):
   ID_code='0'
