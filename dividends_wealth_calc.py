a26390915912e552b50831c9efe08d7001ec9541
James Yang <78498381+polaryang@users.noreply.github.com>
2023-06-28 00:12:03 +0800

    Update dividends_wealth_calc.py

--- dividends_wealth_calc.py
+++ dividends_wealth_calc.py
@@ -312,6 +312,7 @@ with col2:
     st.write('Dividends Rate ($ per share): **'+str(max_divid)+ '**.   Dividend Yield (%): **'+str(round(max_divid/last_close*100,2))+'**.')
     df=divid_cf_calc(age,income_a,income_g,expense_a,inflation,idir,
           divid_rate,last_close,invest_p,divid_live_p,redempt)
+    df_max=df
     deficit=len(df[df['Net_Income']<0])
     #st.write(deficit)
     #st.dataframe(deficit)
@@ -339,6 +340,7 @@ with col2:
     st.write('Dividends Rate ($ per share): **'+str(avg_divid)+ '**   Dividend Yield: **'+str(round(avg_divid/last_close*100,2))+'**')
     df=divid_cf_calc(age,income_a,income_g,expense_a,inflation,idir,
           divid_rate,last_close,invest_p,divid_live_p,redempt)
+    df_avg=df
     deficit=len(df[df['Net_Income']<0])
     #st.write(deficit)
     #st.dataframe(deficit)
@@ -366,6 +368,7 @@ with col2:
     st.write('Dividends Rate ($ per share): **'+str(min_divid)+ '**   Dividend Yield (%): **'+str(round(min_divid/last_close*100,2))+'**')
     df=divid_cf_calc(age,income_a,income_g,expense_a,inflation,idir,
           divid_rate,last_close,invest_p,divid_live_p,redempt)
+    df_min=df
     deficit=len(df[df['Net_Income']<0])
     #st.write(deficit)
     #st.dataframe(deficit)
@@ -398,6 +401,7 @@ with col2:
     #st.write('Min Dividends Rate ($ per share): '+str(min_divid) )
     df=divid_cf_calc(age,income_a,income_g,expense_a,inflation,idir,
           divid_rate,last_close,invest_p,divid_live_p,redempt)
+    df_self=df
     deficit=len(df[df['Net_Income']<0])
     #st.write(deficit)
     #st.dataframe(deficit)
@@ -426,6 +430,11 @@ email_message=st.text_area('留言備註')
 email_message=email_message+'\n客戶信箱:'+email_receiver+'\n年紀:'+str(age)+'\n投資期間:'+str(invest_p)+'\n收成期間:'+str(divid_live_p)+'\n每月薪資:'+str(income)+'\n薪資年成長率:'+str(income_g)+'\n年終獎金:'
 email_message=email_message+str(income_bonus)+'\n每月生活開銷:'+str(expense)+'\n年通貨膨脹率:'+str(inflation)+'\n投資佔可支配所得率:'+str(idir)+'\n可動用存股:'+str(redempt)+'\n投資標的:'+ID_name+':'+stock_ticker
 if st.button('Send Email'):
+  with pd.ExcelWriter('output.xlsx') as writer:  
+    df_max.to_excel(writer, sheet_name='max')
+    df_avg.to_excel(writer, sheet_name='avg')
+    df_min.to_excel(writer, sheet_name='min')
+    df_self.to_excel(writer, sheet_name='self')
   send_email('polaryang@gmail.com', 'ryxbncdvmgncqepk', email_receiver, 'smtp.gmail.com', 587, email_message, '財富自由客戶需求', attachment=None)
 st.write(':gem:*POWERED by* :blue[銘傳大學:dove_of_peace:財務金融學系 金融科技實驗室 楊重任副教授 / 團隊學生: 黃冠斌、姚岱均]')    
     
