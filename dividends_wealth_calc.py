5c08439d71bf4349f5b14a8715b943b2b4b5bc8f
James Yang <78498381+polaryang@users.noreply.github.com>
2023-06-28 10:07:45 +0800

    Update dividends_wealth_calc.py

--- dividends_wealth_calc.py
+++ dividends_wealth_calc.py
@@ -17,13 +17,15 @@ import os
 # ------------------------------------------------------------------
 def send_email(sender, password, receiver, smtp_server, 
 smtp_port, email_message, subject, attachment=None):  
+  #https://zhuanlan.zhihu.com/p/109551738
   message = MIMEMultipart()
   message['To'] = Header(receiver)
   message['From']  = Header(sender)
   message['Subject'] = Header(subject)
   message.attach(MIMEText(email_message,'plain', 'utf-8'))
   if attachment:
-    att = MIMEApplication(attachment.read(), _subtype="txt")
+    att== MIMEApplication(open(file, 'rb').read())
+    #att = MIMEApplication(attachment.read(), _subtype="txt")
     att.add_header('Content-Disposition', 'attachment', filename=attachment.name)
     message.attach(att)
   server = smtplib.SMTP(smtp_server, smtp_port)
