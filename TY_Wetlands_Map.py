3dab6f0e84dab1b98654d66e092db810bc906e49
James Yang <78498381+polaryang@users.noreply.github.com>
2023-08-28 21:45:10 +0800

    Update TY_Wetlands_Map.py

--- TY_Wetlands_Map.py
+++ TY_Wetlands_Map.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 #in_file='D:\James\Research\ESG\濕地\TY_Wetlands.xlsx'
-in_file='https://github.com/polaryang/streamlit-example/raw/master/TY_Wetlands.xlsx'
+in_file='https://github.com/polaryang/streamlit-example/raw/master/TY_Wetlands1.xlsx'
 df=pd.read_excel(in_file)
 #for i in range(len(df)):
 #    print(df['緯度'][i],df['經度'][i])
