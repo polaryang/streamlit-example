f1b4e65b1b2e62fc174bf2661a417c28bb5b70eb
James Yang <78498381+polaryang@users.noreply.github.com>
2023-08-28 23:23:45 +0800

    Update TY_Wetlands_Map.py

--- TY_Wetlands_Map.py
+++ TY_Wetlands_Map.py
@@ -12,5 +12,5 @@ choice_list=['全部', '水鳥度冬區', '野生動物保護區', '野生動物
 df=pd.read_excel(in_file, sheet_name='data')
 #for i in range(len(df)):
 #    print(df['緯度'][i],df['經度'][i])
-st.map(df, latitude='緯度',  longitude='經度', size=500, color='選點原因Color')
+st.map(df, latitude='緯度',  longitude='經度', size=100, color='選點原因Color')
 #print(df)
