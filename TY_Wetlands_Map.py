869763c68b31a19f4c9d8941fd10273f54b6279e
James Yang <78498381+polaryang@users.noreply.github.com>
2023-08-28 23:14:04 +0800

    Update TY_Wetlands_Map.py

--- TY_Wetlands_Map.py
+++ TY_Wetlands_Map.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 st.set_page_config(page_title='桃園埤圳重要濕地（國家級）資料庫', page_icon=':sparkles:', layout='wide')
 st.header(':sparkles: 桃園埤圳重要濕地（國家級）資料地圖')
-st.markdown('水鳥度冬區 野生動物保護區 環境教育區 生物多樣性較高')
+st.header(':yellow[水鳥度冬區] :blue[野生動物保護區] :green[環境教育區] :violet[生物多樣性較高]')
 
 
 #in_file='D:\James\Research\ESG\濕地\TY_Wetlands.xlsx'
@@ -14,6 +14,6 @@ st.write('You selected:', options)
 df=pd.read_excel(in_file, sheet_name='data')
 #for i in range(len(df)):
 #    print(df['緯度'][i],df['經度'][i])
-st.map(df, latitude='緯度',  longitude='經度', color='選點原因Color')
+st.map(df, latitude='緯度',  longitude='經度', size=100, color='選點原因Color')
 print(df)
  
